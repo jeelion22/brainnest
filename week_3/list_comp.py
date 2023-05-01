36d2ba142929b281fc8004778c0a5d25808b179b
jeeva m <jeelion22@gmail.com>
2023-05-01 15:50:00 +0530

    Revert "Add week_3 folder to week_2_assignment branch"
    This reverts commit c9a523ea2a082bfcf2447d05819b441837b6dfa4.

--- week_3/list_comp.py
+++
@@ -1,9 +0,0 @@
-"""
-1.Write a list comprehension to find all the numbers 
-between 1 and 1000 that are divisible by 7
- but not by 5
-"""
-
-lst = [i for i in range(1000) if i % 7 == 0 if i % 5]
-
-print(lst)
